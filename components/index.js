d1c7e3a749d149e0ca61b1732c7aa10d0bab9692
Phahladira-Moja <phahladira@patala.co.za>
2023-07-06 13:03:00 +0200

    Completed the Details page as well as the details bid component

--- components/index.js
+++ components/index.js
@@ -3,5 +3,17 @@ import FocusedStatusBar from "./FocusedStatusBar";
 import NftCard from "./NftCard";
 import { CircleButton } from "./Button";
 import { RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
 
-export { HomeHeader, FocusedStatusBar, NftCard, RectButton, CircleButton };
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NftCard,
+  RectButton,
+  CircleButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
